3bf160fc8ad3ff5deda283c4cd7a994e357ff379
CMJ <meckel_cmj@163.com>
2018-07-18 19:59:40 +0800

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
 
+use Auth;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -57,7 +59,51 @@ class User extends Authenticatable
     //将当前用户发布过的所有微博从数据库取出,并按时间倒序
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+
+        array_push($user_ids, Auth::user()->id);
+
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
     }
+
+    //获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    //获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    //微博关注
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids,false);
+    }
+
+    //微博取消关注
+    public function unfollow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断当前登录用户A是否关注了用户B
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+
+    }
+
+
 
 }
