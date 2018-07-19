7c97eddc1c73aaf6a26f3928d1b731fdc4d2bfb4
CMJ <meckel_cmj@163.com>
2018-07-19 14:41:27 +0800

    修改默认字符串长度,兼容MYSQL5.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Carbon\Carbon;
 
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Carbon::setLocale('zh');
     }
 
